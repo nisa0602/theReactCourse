557401d0f53c8ba9cb4420165920911d2d7e3515
nisa0602 <nisahidayatika999@gmail.com>
2022-10-26 19:28:41 +0700

    lec 18

---
+++ section1/18-practiceClassArr/app.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+        icon: "code",
+        title: "Development"
+    },
+    {
+        icon: "business",
+        title: "Business"
+    },
+    {
+        icon: "computer",
+        title: "IT & Software"
+    },
+    {
+        icon: "library_books",
+        title: "Office Productivity"
+    },
+    {
+        icon: "person",
+        title: "Personal Development"
+    },
+    {
+        icon: "mode_edit",
+        title: "Personal Development"
+    },    
+    {
+        icon: "track_changes",
+        title: "Marketing"
+    },    
+    {
+        icon: "directions_bike",
+        title: "Health & Fitness"
+    },    
+    {
+        icon: "music_note",
+        title: "Music"
+    },
+]
+
+// let icons = data.map((i, icon, title) => {
+//     return(
+//         <Icon key={i} icon={icon} title={title}/>
+//     )
+// });
+
+// console.log(icons)
+
+ReactDOM.render(
+   <CatNav data={data}/>,
+   document.getElementById('root')
+);
